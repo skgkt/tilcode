ef524e61178c6179bf4ada992e942bdeb4ae2e09
Kenta Sakaguchi <keonthae@gmail.com>
2021-03-27 14:50:35 +0900

    style:コード整形実施

--- lazy-initialization/src/test/java/app/ApplicationTest.java
+++ lazy-initialization/src/test/java/app/ApplicationTest.java
@@ -1,15 +1,16 @@
 package app;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest
 class ApplicationTest {
-    @Autowired @Qualifier("bean1")
+    @Autowired
+    @Qualifier("bean1")
     private String sut;
 
     @Test

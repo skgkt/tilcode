f0830f9a200cc70458fbbfe3d27f24bb7c06c880
Kenta Sakaguchi <keonthae@gmail.com>
2021-03-27 14:37:21 +0900

    build:タスク設定の書き方を改善

--- lazy-initialization/build.gradle.kts
+++ lazy-initialization/build.gradle.kts
@@ -17,7 +17,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.test {
     systemProperty("spring.profiles.active", "test")
 }
